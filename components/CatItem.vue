cd966ce9450f9d9a1778217a4f21bb7101a4b820
s0214m <m.suguru.0214@gmail.com>
2022-06-06 05:33:13 +0900

    no message

---
+++ components/CatItem.vue
@@ -0,0 +1,71 @@
+<template>
+  <v-col cols="12" sm="12" md="12" lg="6" xl="6" :class="$style.container">
+    <div :class="$style.cat_unit">
+      <img
+        :class="[
+          $style.cat_img,
+          { [$style.small]: $vuetify.breakpoint.mdAndDown },
+        ]"
+        :src="cat.url"
+        :alt="cat.uuid"
+      />
+      <div :class="$style.figure">
+        <div>
+          <span :class="$style.figure1">撮影場所：</span>
+          <span>{{ cat.address }}</span>
+        </div>
+        <div>
+          <span :class="$style.figure2">撮影日：</span>
+          <span>{{ cat.createDate | moment }}</span>
+        </div>
+      </div>
+    </div>
+  </v-col>
+</template>
+
+<script>
+export default {
+  props: {
+    cat: {
+      type: Object,
+      default: null,
+    },
+  },
+}
+</script>
+
+<style lang="scss" module>
+.container {
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+  border-bottom: 1px solid #ddd;
+  .cat_unit {
+    .cat_img {
+      text-align: center;
+      max-width: 100%;
+      height: 500px;
+      margin-bottom: 15px;
+      object-fit: contain;
+      &.small {
+        height: unset;
+        max-height: 500px;
+      }
+    }
+    .figure {
+      width: fit-content;
+      margin: 0 auto;
+      .figure1 {
+        font-weight: bold;
+        width: 80px;
+        display: inline-block;
+      }
+      .figure2 {
+        font-weight: bold;
+        width: 80px;
+        display: inline-block;
+      }
+    }
+  }
+}
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -24,7 +24,9 @@ export default {
   css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: [
+    { src: '~/plugins/moment-filter', ssr: false }
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -45,6 +47,7 @@ export default {
     '@nuxtjs/pwa',
     // https://go.nuxtjs.dev/content
     '@nuxt/content',
+    ['@nuxtjs/moment', ['ja']],
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
